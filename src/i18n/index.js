0532edbda39813db6b209ed3bd21326e9303ab92
Oliver Mak <oliver139.working@gmail.com>
2022-11-12 22:52:41 +0800

    Minor fix

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,6 @@
 import { createI18n } from "vue-i18n";
 import messages from "./lang";
-console.log(localStorage.site_lang);
+
 const i18n = createI18n({
   // default locale
   locale: localStorage.site_lang || "en",
