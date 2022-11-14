1098243d292f5744d38e0e3bf1f340bd8f8ef136
Oliver Mak <oliver139.working@gmail.com>
2022-11-15 00:22:11 +0800

    Animxyz for fast coding transition animations

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
 import i18n from "./i18n";
+import VueAnimXyz from "@animxyz/vue3";
+import "@animxyz/core";
 
 import "./assets/css/main.scss";
 import "./assets/css/color.scss";
 
-createApp(App).use(router).use(i18n).mount("#app");
+createApp(App).use(router).use(i18n).use(VueAnimXyz).mount("#app");
