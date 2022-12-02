1ae2b2b52b90b34ed93fe8fe842220ba61be491f
Oliver Mak <oliver139.working@gmail.com>
2022-12-03 00:08:27 +0800

    Favicon & website title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "みけねこお誕生日記念計画2023 | みけねこの貓窩",
+    },
+  }
 });
