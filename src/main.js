a6454b4e9c6dbf2d79a795d9f75677c3f429a3e4
Oliver Mak <oliver139.working@gmail.com>
2022-12-03 00:42:27 +0800

    Icon library

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ import i18n from "./i18n";
 import VueAnimXyz from "@animxyz/vue3";
 import "@animxyz/core";
 
+// icons
+import OhVueIcon from "./utils/iconImport";
+
 import "./assets/css/main.scss";
 import "./assets/css/color.scss";
 
-createApp(App).use(router).use(i18n).use(VueAnimXyz).mount("#app");
+createApp(App)
+  .use(router)
+  .use(i18n)
+  .use(VueAnimXyz)
+  .component("v-icon", OhVueIcon)
+  .mount("#app");
---
+++ src/utils/iconImport.js
@@ -0,0 +1,13 @@
+// Oh Vue Icon https://oh-vue-icons.js.org/
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import {
+  FaFlag,
+  RiZhihuFill
+} from "oh-vue-icons/icons";
+
+addIcons(
+  FaFlag,
+  RiZhihuFill
+);
+
+export default OhVueIcon;
