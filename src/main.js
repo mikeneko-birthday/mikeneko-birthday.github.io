9fd308f5982b7b5261c0c624005c186dd1b98aa0
Oliver Mak <oliver139.working@gmail.com>
2022-12-04 03:01:04 +0800

    AnimXYZ not working great

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 
 import i18n from "./i18n";
-import VueAnimXyz from "@animxyz/vue3";
-import "@animxyz/core";
 
 // icons
 import OhVueIcon from "./utils/iconImport";
@@ -15,6 +13,5 @@ import "./assets/css/color.scss";
 createApp(App)
   .use(router)
   .use(i18n)
-  .use(VueAnimXyz)
   .component("v-icon", OhVueIcon)
   .mount("#app");
