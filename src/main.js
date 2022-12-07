e2ac6493310a499b17a6cc3ce055ecd0f6fccf06
Oliver Mak <oliver139.working@gmail.com>
2022-12-08 02:26:04 +0800

    Global register mitt

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import mitt from "mitt";
 import i18n from "./i18n";
 
 // icons
@@ -11,8 +12,11 @@ import "./assets/css/main.scss";
 import "./assets/css/color.scss";
 import "./assets/css/transition.scss";
 
-createApp(App)
-  .use(router)
+const emitter = mitt();
+const app = createApp(App);
+
+app.config.globalProperties.$emitter = emitter;
+app.use(router)
   .use(i18n)
   .component("v-icon", OhVueIcon)
   .mount("#app");
