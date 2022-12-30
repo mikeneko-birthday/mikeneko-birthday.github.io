ed802c02ea6d3559bc93614e0d92f46bff194c92
Oliver Mak <oliver139.working@gmail.com>
2022-12-30 23:49:39 +0800

    Hide content scrollbar for beautiful

--- src/components/SiteComponents/SiteContent.vue
+++ src/components/SiteComponents/SiteContent.vue
@@ -23,6 +23,13 @@ export default {
 <style lang="scss">
 .site-content {
   height: 100%;
+  overflow-y: scroll;
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none;  /* Internet Explorer 10+ */
+  &::-webkit-scrollbar { /* WebKit */
+      width: 0;
+      height: 0;
+  }
   &.fade-white-bg .view {
     background: rgba(255, 255, 255, .8);
     border-radius: calc(var(--nav-gap-width, 0.625rem) + var(--nav-item-size, 4rem) / 2);
