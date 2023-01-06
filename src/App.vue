22ecc551c4c1b4d15a91fcbabce884d512c1aa99
Oliver Mak <oliver139.working@gmail.com>
2023-01-07 02:52:11 +0800

    Add dvh support

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@ export default {
 #app {
   --header-height: 8.125rem;
   height: 100vh;
+  height: 100dvh;
   background: #e0b4bf url("@/assets/img/background.svg") repeat center;
   background-size: 350px;
   &.cat-paw {
