f77c11ee8a75ec08714f8c7b1c6a3d0d6ff16472
Oliver Mak <oliver139.working@gmail.com>
2023-01-07 03:05:26 +0800

    Drop console log on build

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,19 @@
+const prodPlugin = [];
+
+if (process.env.NODE_ENV === "production") {
+  prodPlugin.push([
+    "transform-remove-console",
+    {
+      exclude: ["error", "warn"],
+    },
+  ]);
+}
+
 module.exports = {
   presets: [
     "@vue/cli-plugin-babel/preset"
+  ],
+  plugins: [
+    ...prodPlugin
   ]
 };
