6da811ef98a1797ca5dc0759e1000858268b4f98
Oliver Mak <oliver139.working@gmail.com>
2023-01-06 21:53:48 +0800

    Route unmatch path to home

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
   },
   {
+    path: "/illustration",
+    name: "illustration",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/IllustrationView.vue")
+  },
+  {
     path: "/message",
     name: "message",
     // route level code-splitting
@@ -23,6 +31,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/MessageView.vue")
   },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/",
+  }
 ];
 
 const router = createRouter({
