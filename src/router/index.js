ac60adac83f539f6e27283ed52db988d24dbd7a7
Oliver Mak <oliver139.working@gmail.com>
2023-01-07 16:36:55 +0800

    Fix chunk

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/IllustrationView.vue")
+    component: () => import(/* webpackChunkName: "illustration" */ "../views/IllustrationView.vue")
   },
   {
     path: "/message",
@@ -29,7 +29,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/MessageView.vue")
+    component: () => import(/* webpackChunkName: "message" */ "../views/MessageView.vue")
   },
   {
     path: "/:pathMatch(.*)*",
