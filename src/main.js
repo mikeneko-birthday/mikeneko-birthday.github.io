8420ff29c546549f0470de8849cf927f50162636
Oliver Mak <oliver139.working@gmail.com>
2023-01-09 01:51:59 +0800

    Install v-viewer

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import i18n from "./i18n";
 // icons
 import OhVueIcon from "./utils/iconImport";
 
+// v-viewer
+import "viewerjs/dist/viewer.css";
+import VueViewer from "v-viewer";
+
 import "./assets/css/main.scss";
 import "./assets/css/color.scss";
 import "./assets/css/transition.scss";
@@ -20,5 +24,6 @@ app.config.globalProperties.$emitter = emitter;
 app
   .use(router)
   .use(i18n)
+  .use(VueViewer)
   .component("v-icon", OhVueIcon)
   .mount("#app");
