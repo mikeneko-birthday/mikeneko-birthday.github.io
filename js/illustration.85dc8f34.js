aba39e967da655041103a3213da0a4aea692061f
oliver139 <oliver139@users.noreply.github.com>
2023-01-10 21:11:39 +0000

    deploy: ce254142e1bb3391b60ffe95b8882f19f6d4aeb3

---
+++ js/illustration.85dc8f34.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmikeneko_birthday"]=self["webpackChunkmikeneko_birthday"]||[]).push([[673],{2367:function(n,t,e){e.r(t),e.d(t,{default:function(){return l}});var u=e(3396);const o=(0,u._)("section",{class:"view view-about"},[(0,u._)("h1",null,"Illustration")],-1);function i(n,t,e,i,r,s){const a=(0,u.up)("SiteContent");return(0,u.wg)(),(0,u.j4)(a,null,{default:(0,u.w5)((()=>[o])),_:1})}var r=e(836),s={components:{SiteContent:r.Z}},a=e(89);const c=(0,a.Z)(s,[["render",i]]);var l=c}}]);
+//# sourceMappingURL=illustration.85dc8f34.js.map
