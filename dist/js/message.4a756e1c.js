73cbb3ce58b27048ba1324272cd65752180fa804
Oliver Mak <oliver139.working@gmail.com>
2023-01-10 23:09:28 +0800

    Remove dist

--- dist/js/message.4a756e1c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmikeneko_birthday"]=self["webpackChunkmikeneko_birthday"]||[]).push([[676],{605:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var s=t(3396);const u=(0,s._)("section",{class:"view view-messages"},[(0,s._)("h1",null,"Messages")],-1);function i(e,n,t,i,o,r){const a=(0,s.up)("SiteContent");return(0,s.wg)(),(0,s.j4)(a,null,{default:(0,s.w5)((()=>[u])),_:1})}var o=t(5981),r={components:{SiteContent:o.Z}},a=t(89);const c=(0,a.Z)(r,[["render",i]]);var l=c}}]);
-//# sourceMappingURL=message.4a756e1c.js.map
