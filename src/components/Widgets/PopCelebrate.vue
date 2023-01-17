fc6dca853d3222c8a646e151b96401840043ab45
Oliver Mak <oliver139.working@gmail.com>
2023-01-17 19:46:37 +0800

    Styling

--- src/components/Widgets/PopCelebrate.vue
+++ src/components/Widgets/PopCelebrate.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="model-celebrate">
-    <img src="@/assets/img/drawings/mikeneko_bd2.png" alt="Happy Birthday to Mikeneko">
+    <div class="msg-holder">
+      <img src="@/assets/img/drawings/mikeneko_bd2.png" alt="Happy Birthday to Mikeneko">
+      <div class="msg msg-bottom">Happy Birthday</div>
+    </div>
   </div>
 </template>
 
@@ -12,13 +15,51 @@ export default {
 
 <style lang="scss" scoped>
 .model-celebrate {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: fixed;
   inset: 0;
   background: rgba(0, 0, 0, .8);
   z-index: 10;
-  img {
-    height: 80vh;
-    height: 80dvh;
+  .msg-holder {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    .msg {
+      color: #FFC000;
+      font-size: 10vw;
+      font-family: "Dancing Script", cursive;
+      text-align: center;
+      text-shadow: 0 0 .5rem #acacac;
+      position: absolute;
+      bottom: 2rem;
+      width: 100%;
+    }
+  }
+  @media screen and (max-width: 48em) {
+    .msg-holder {
+      img {
+        max-width: none;
+        max-height: 80vh;
+      }
+      .msg {
+        font-size: 12vw;
+      }
+    }
+  }
+  @media screen and (max-width: 30em) {
+    .msg-holder {
+      img {
+        max-height: 70vh;
+      }
+      .msg {
+        font-size: 18vw;
+        word-spacing: 100rem;
+      }
+    }
   }
 }
 </style>
