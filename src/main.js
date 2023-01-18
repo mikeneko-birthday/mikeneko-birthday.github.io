ea6ed92e5de77db3afcb39c330434921889907e1
Oliver Mak <oliver.mak@digiqal.com>
2023-01-18 12:55:38 +0800

    Try Google Analytics

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ import VueViewer from "v-viewer";
 // Masonry
 import MasonryWall from "@yeger/vue-masonry-wall";
 
+// Google Analytics
+import VueGtag from "vue-gtag";
+
 import "./assets/css/main.scss";
 import "./assets/css/color.scss";
 import "./assets/css/transition.scss";
@@ -29,5 +32,10 @@ app
   .use(i18n)
   .use(VueViewer)
   .use(MasonryWall)
+  .use(VueGtag, {
+    config: {
+      id: "G-81YR852LSZ"
+    },
+  }, router)
   .component("v-icon", OhVueIcon)
   .mount("#app");
